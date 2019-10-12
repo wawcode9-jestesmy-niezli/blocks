1d237972163b45899ec4cfb0d3f8283af87a8509
Marcin Rakowski <marcin.rakowski@dreamlab.pl>
2019-10-12 15:25:31 +0200

    use game service

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
+import usePostGameService from "./services/Game";
+import {Request} from "./types/Service";
 
 const App: React.FC = () => {
-
+    const service = usePostGameService();
     return (
-        <div className="App">
-
+        <div>
+            {service.status === Request.LOADING && <div>Loading...</div>}
+            {service.status === Request.LOADED &&
+            service.payload.blocks.map(block => (
+                <div key={block.originPosition}>{block.originPosition}</div>
+            ))}
+            {service.status === Request.ERROR && (
+                <div>Error, the backend moved to the dark side.</div>
+            )}
         </div>
     );
 };
