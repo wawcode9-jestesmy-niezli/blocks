f3d7e08f916fb4723fdb8b396b43682522fc6a0c
Marcin Rakowski <marcin.rakowski@dreamlab.pl>
2019-10-13 13:58:49 +0200

    game move count

--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -61,7 +61,7 @@ const Block: React.FC<CardProps> = ({block, selected, fnClick}) => {
     return (
         <Grid item xs={2} key={block.image}
               className={getClassName()}
-              style={{backgroundImage: `url('/test.jpg')`}}
+              style={{backgroundImage: `url('http://localhost:3000/test.jpg')`}}
               onClick={() => fnClick(block)}>
             {block.activePosition} - {block.originPosition}
         </Grid>
